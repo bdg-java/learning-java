f88fe2c4b245e08fcbd835d6374c695d44f920ec
William Arustamyan <williamarustamyan@workfront.com>
2019-05-04 13:51:40 +0400

    IF condition

--- src/com/bdg/Application.java
+++ src/com/bdg/Application.java
@@ -1,6 +1,7 @@
 package com.bdg;
 
 
+
 /**
  * @author William Arustamyan
  */
@@ -10,11 +11,22 @@ public class Application {
 
     public static void main(String[] args) {
 
+        int a = 333333333;
+        boolean b = false;
+        char c = 'c';
+        long L = 2343434;
+        double d = 34.5;
+        float f = 556.7f;
+        byte fff = 127;
+        short sh = 443;
 
-        int x = 10;
-        int y = 20;
-        int z = x + y;
 
-        System.out.println("Z : " + z);
+
+
+        if (a < 10) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
     }
 }
