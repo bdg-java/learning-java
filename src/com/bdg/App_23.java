45b85c709930449743132dd082b378d0ae90ccfb
Nuard <nuard@atabekyan.com>
2019-05-05 18:37:20 +0400

    App 23

--- src/com/bdg/App_23.java
+++ src/com/bdg/App_23.java
@@ -7,7 +7,7 @@ public class App_23 {
         int c = 8;
 
         if (a == 1 || b == 1 || c == 1) {
-            System.out.print("TRUE");
+            System.out.print("TRUE ");
         } else {
             System.out.print("FALSE");
         }
