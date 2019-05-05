ad9cb5d3562bb4c111e5fabb409b2314b995384e
Nuard <nuard@atabekyan.com>
2019-05-05 20:17:33 +0400

    App 26-28

---
+++ src/com/bdg/App_27.java
@@ -0,0 +1,17 @@
+package com.bdg;
+
+public class App_27 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+        int c = 5;
+        int d1 = b - a;
+        int d2 = c - b;
+
+        if (d1 == d2 ) {
+            System.out.print("TRUE");
+        } else {
+            System.out.print("FALSE");
+        }
+    }
+}
---
+++ src/com/bdg/App_28.java
@@ -0,0 +1,14 @@
+package com.bdg;
+
+public class App_28 {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 4;
+        int q1 = b/a, q2 = c/b;
+
+        if (q1 == q2) {
+            System.out.print("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+    }
+}
