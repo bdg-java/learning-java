2d4066157061f0d04d5bdf43b496cdee498bf22e
Nuard <nuard@atabekyan.com>
2019-05-05 19:02:58 +0400

    App 24

---
+++ src/com/bdg/App_24.java
@@ -0,0 +1,15 @@
+package com.bdg;
+
+public class App_24 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = 1;
+
+        if ((a == 2 && b == 2) || (a == 2 && c == 2) || (b == 2 && c == 2)){
+            System.out.print("TRUE");
+        } else {
+            System.out.print("FALSE");
+        }
+    }
+}
