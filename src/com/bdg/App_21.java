91e96a50bc4ec8b05670b81e14cf7e9c63bbcfa8
Nuard <nuard@atabekyan.com>
2019-05-05 18:20:15 +0400

    App 21

--- src/com/bdg/App_21.java
+++ src/com/bdg/App_21.java
@@ -2,4 +2,23 @@ package com.bdg;
 
 public class App_21 {
 
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println("a is the largest: " + a);
+            } else {
+                System.out.println("c is the largest: " + c);
+            }
+        } else {
+            if (b > c) {
+                System.out.println("b is the largest: " + b);
+            } else {
+                System.out.println("c is the largest: " + c);
+            }
+        }
+    }
 }
