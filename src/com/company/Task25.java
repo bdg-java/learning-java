a3ccae52d18747406834068592923f08e7cb99ab
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 02:29:48 +0400

    Task 25 done

---
+++ src/com/company/Task25.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Task25 {
+    public static void main(String[] args) {
+        int a = 5, b = 20, c = 6;
+        if (a + b <= c || a + c <= b || b + c <= a)
+            System.out.println("y = 2");
+        else System.out.println("y = 1");
+    }
+}
