cc9096c58c0bde588daf9ab9206202c4177d5fbc
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-06 21:30:24 +0400

    Task 21 is done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 int a = 600,  b = 700,  c = 400;
+	 int a = 600,  b = 700,  c = 800;
 	 if(a > b && a > c)
 	     System.out.println("First is max : " + a);
 	 else if (b > a && b > c)
