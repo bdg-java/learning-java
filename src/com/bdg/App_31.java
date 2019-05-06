178a8f4069f4da27cbb4e8dca94b76a4fa613b53
Nuard <nuard@atabekyan.com>
2019-05-06 22:27:40 +0400

    App 31-32

---
+++ src/com/bdg/App_31.java
@@ -0,0 +1,37 @@
+package com.bdg;
+
+public class App_31 {
+    public static void main(String[] args) {
+        int a = 17, b = 11, c = 10, d = 9;
+
+        if (a > b) {
+            if (a > c) {
+                if (a > d) {
+                    System.out.println("a is the largest: " + a);
+                } else {
+                    System.out.println("d is the largest: " + d);
+                }
+            } else {
+                if (c > d) {
+                    System.out.println("c is the largest: " + c);
+                } else {
+                    System.out.println("d is the largest: " + d);
+                }
+            }
+        } else {
+            if (b > c) {
+                if (b > d) {
+                    System.out.println("b is the largest: " + b);
+                } else {
+                    System.out.println("d is the largest: " + d);
+                }
+            } else {
+                if (c > d) {
+                    System.out.println("c is the largest: " + c);
+                } else {
+                    System.out.println("d is the largest: " + d);
+                }
+            }
+        }
+    }
+}
---
+++ src/com/bdg/App_32.java
@@ -0,0 +1,37 @@
+package com.bdg;
+
+public class App_32 {
+    public static void main(String[] args) {
+        int a = 7, b = 11, c = 10, d = 9;
+
+        if (a < b) {
+            if (a < c) {
+                if (a < d) {
+                    System.out.println("a is min: " + a);
+                } else {
+                    System.out.println("d is min: " + d);
+                }
+            } else {
+                if (c < d) {
+                    System.out.println("c is min: " + c);
+                } else {
+                    System.out.println("d is min: " + d);
+                }
+            }
+        } else {
+            if (b < c) {
+                if (b < d) {
+                    System.out.println("b is min: " + b);
+                } else {
+                    System.out.println("d is min: " + d);
+                }
+            } else {
+                if (c < d) {
+                    System.out.println("c is min: " + c);
+                } else {
+                    System.out.println("d is min: " + d);
+                }
+            }
+        }
+    }
+}
