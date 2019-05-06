43f36030f30af09acffc391b8fdd332e6b5d3bb0
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-06 20:49:59 +0400

    Task 21 is done

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int a = 600,  b = 700,  c = 400;
+	 if(a > b && a > c)
+	     System.out.println("First is max : " + a);
+	 else if (b > a && b > c)
+         System.out.println("Second is max : " + b);
+	 else
+	     System.out.println("Third is max : " + c);
+
+
+    }
+}
