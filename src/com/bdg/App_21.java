15bce9fa5dcabb31565b0bfaebd97fffca5c5b68
AsArabyan <ahhn-93@mail.ru>
2019-05-07 00:50:08 +0400

    AsArabyan: App_21

---
+++ src/com/bdg/App_21.java
@@ -0,0 +1,26 @@
+package com.bdg;
+
+public class App_21 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 8;
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println("a is the maximum: " + a);
+            } else {
+                System.out.println("c is the maximum: " + c);
+            }
+        } else {
+            if (b > c) {
+                System.out.println("b is the maximum: " + b);
+            } else {
+                System.out.println("c is the maximum: " + c);
+
+            }
+            }
+        }
+    }
+
