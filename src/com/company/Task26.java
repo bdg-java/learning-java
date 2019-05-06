d8c771233097ed59ddf51df9b27b3b1f96f698cb
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 01:37:18 +0400

    Task 26 done

---
+++ src/com/company/Task26.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Task26 {
+    public static void main(String[] args) {
+        int a = 27 , b = 5, c = 18;
+            if((a&1) == 0 || (b&1) == 0 || (c&1) == 0)
+                System.out.println(1);
+            else
+                System.out.println(2);
+    }
+}
