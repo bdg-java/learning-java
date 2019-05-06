74a57e1c7be89c4adef924cd589e89fd5af679f6
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 02:14:41 +0400

    Task 27 done

---
+++ src/com/company/Task28.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Task28 {
+    public static void main(String[] args) {
+        int a= 2, b = 6, c = 18;
+        if ( (float)b / a == (float)c / b)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
