536f33a6a9e081d7da854cc5a86c5ecb49ee6b7d
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-05-06 21:23:00 +0400

    34,35,36

---
+++ src/com/bdg/App_34.java
@@ -0,0 +1,18 @@
+package com.bdg;
+
+public class App_34 {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 35;
+        int c = 75;
+        int d = 44;
+
+        if ((a + b == c + d)||(a + c == b + d)|| (a + d == b + c)) {
+            System.out.println("TRUE");
+        }
+        else {
+            System.out.println("FALSE");
+        }
+    }
+}
---
+++ src/com/bdg/App_36.java
@@ -0,0 +1,18 @@
+package com.bdg;
+
+public class App_36 {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 35;
+        int c = 75;
+        int d = 44;
+
+        if ((a % 2 == 0 && b % 2 == 0)||(a % 2 == 0 && c % 2 == 0)||(a % 2 == 0 && d % 2 == 0)||(b % 2 == 0 && c % 2 == 0)||(b % 2 == 0 && d % 2 == 0)||(c % 2 == 0 && d % 2 == 0)){
+            System.out.println("2");
+        }
+        else {
+            System.out.println(1);
+        }
+    }
+}
