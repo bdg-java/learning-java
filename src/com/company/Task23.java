9e532cd959be7c01951e8d35ab4405eb5d9d3c80
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 02:21:54 +0400

    Task 23 done

---
+++ src/com/company/Task23.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Task23 {
+    public static void main(String[] args) {
+        int a = 12, b = 12, c = 20;
+        if (a == 1 || b == 1 || c == 1)
+            System.out.println("True");
+        else System.out.println("False");
+    }
+}
