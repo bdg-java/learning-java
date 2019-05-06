1b0f905f9ca5374270a708fa3e1233d3a2445742
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 02:03:13 +0400

    Task 27 done

---
+++ src/com/company/Task27.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Task27 {
+    public static void main(String[] args) {
+        int a= 2, b = 5, c = 9;
+        if ( b - a == c - b)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+
+}
