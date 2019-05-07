1b5a6461fedce554c8e8a42bc36f0f07bf48f6ac
AGharibyan <anigharib@gmail.com>
2019-05-08 03:38:01 +0500

    AGharibyan App_33

---
+++ src/com/bdg/App_33.java
@@ -0,0 +1,30 @@
+package com.bdg;
+
+public class App_33 {
+
+    public static void main (String [] orgs) {
+
+        int a = 500;
+        int b = 19;
+        int c = 0;
+        int d = 2;
+
+        if ( a == 1 ) {
+            System.out.println("true");
+            } else {
+                if ( b == 1 ) {
+                    System.out.println("true");
+                } else {
+                    if ( c == 1 ) {
+                        System.out.println("true");
+                    } else {
+                        if ( d == 1 ) {
+                            System.out.println("true");
+                        } else {
+                            System.out.println("false");
+                        }
+                    }
+            }
+        }
+    }
+}
