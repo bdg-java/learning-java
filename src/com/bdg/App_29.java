6b0eb87c590652dcca58777d9389f6d25f4c5789
AGharibyan <anigharib@gmail.com>
2019-05-08 01:49:13 +0500

    AGharibyan App_29

--- src/com/bdg/App_29.java
+++ src/com/bdg/App_29.java
@@ -2,23 +2,31 @@ package com.bdg;
 
 public class App_29 {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         int a = 6;
         int b = 1;
         int c = 5;
 
-        if ( a < b ) {
-            if ( b < c ) {
+        if (a < b) {
+            if (b < c) {
                 System.out.println("a , b , c");
             } else {
-                System.out.println("a , c , b");
+                if (a < c) {
+                    System.out.println("a , c , b");
+                } else {
+                    System.out.println("c, a, b");
+                }
             }
         } else {
-            if ( a < c ) {
+            if (a < c) {
                 System.out.println("b , a , c");
             } else {
-                System.out.println("b , c , a");
+                if (b < c) {
+                    System.out.println("b , c , a");
+                } else {
+                    System.out.println("c , b, a");
+                }
             }
         }
     }
