ae953741e205be4bad4232737c92e32c2e870096
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-05-07 13:41:49 +0400

    37-38

---
+++ src/com/bdg/App_38.java
@@ -0,0 +1,21 @@
+package com.bdg;
+
+public class App_38 {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 35;
+        int c = 75;
+        int d = 44;
+        int r2 = (b - a);
+        int r3 = (c - b);
+        int r4 = (d - c);
+
+            if (r2 == r3 && r3 == r4) {
+                System.out.println("TRUE");
+            }
+            else{
+                System.out.println("FALSE");
+            }
+    }
+}
