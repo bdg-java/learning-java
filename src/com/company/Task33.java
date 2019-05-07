76140927d599e13cf62fedea3b2013701f722b95
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-07 20:28:44 +0400

    Task 33 done

---
+++ src/com/company/Task33.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Task33 {
+    public static void main(String[] args) {
+        int a = 1, b = 12, c = 20, d = 5;
+        if (a == 1 || b == 1 || c == 1 || d == 1)
+            System.out.println("True");
+        else System.out.println("False");
+    }
+
+}
