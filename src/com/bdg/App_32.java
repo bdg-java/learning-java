bf6dc45707203d7dd01599f55acff327b7552f19
araqs_sh <araqs.shahbazian@gmail.com>
2019-05-08 19:14:42 +0400

    refactoring to more efficient solutions

--- src/com/bdg/App_32.java
+++ src/com/bdg/App_32.java
@@ -3,14 +3,16 @@ package com.bdg;
 public class App_32 {
     public static void main(String args[]) {
         int a = 37, b = 5, c = 55, d = 96;
-        if (a < b && a < c && a < d) {
-            System.out.println(a);
-        } else if (b < c && b < d) {
-            System.out.println(b);
-        } else if (c < d) {
-            System.out.println(c);
-        } else {
-            System.out.println(d);
+        int min = a;
+        if(b<min){
+            min = b;
         }
+        if(c<min){
+            min = c;
+        }
+        if(d<min){
+            min = d;
+        }
+        System.out.println(min);
     }
 }
