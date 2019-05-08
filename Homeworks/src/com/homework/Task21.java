2342d63e3e679723f1f816de6eca223a34daeb48
Eduard Poghosyan <ed.poghosyan@gmail.com>
2019-05-08 12:11:09 +0400

    TASK 21

---
+++ Homeworks/src/com/homework/Task21.java
@@ -0,0 +1,16 @@
+package com.homework;
+
+import java.util.Scanner;
+
+public class Task21 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+
+    }
+}
