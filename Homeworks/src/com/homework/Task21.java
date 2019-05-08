ce6038dc0bcf06c8cc44253552f351fe52d3a448
Eduard Poghosyan <ed.poghosyan@gmail.com>
2019-05-08 12:27:34 +0400

    Task 21 done

--- Homeworks/src/com/homework/Task21.java
+++ Homeworks/src/com/homework/Task21.java
@@ -4,13 +4,34 @@ import java.util.Scanner;
 
 public class Task21 {
 
+    public static void maxNumber(int a, int b, int c) {
+        int max = 0;
+
+        if (a > b) {
+            if (a > c)
+                System.out.println("The biggest is: " + a);
+            else
+                System.out.println("The biggest is: " + c);
+        }
+        else {
+            if (b < c)
+                System.out.println("The biggest is: " + c);
+            else
+                System.out.println("The biggest is: " + b);
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Insert a");
         int a = scanner.nextInt();
+        System.out.println("Insert b");
         int b = scanner.nextInt();
+        System.out.println("Insert c");
         int c = scanner.nextInt();
 
-
+        maxNumber(a, b, c);
     }
 }
