6c6ce4c725c7e9847a2c0bff43c398b8c8e0424a
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-09 23:11:42 +0400

    Task 66 done

---
+++ src/com/lesson2/Task66.java
@@ -0,0 +1,10 @@
+package com.lesson2;
+
+public class Task66 {
+    public static void main(String[] args) {
+        int a = 4562;
+        if (a / 1000 == 4 && a % 10 == 4)
+            System.out.print("YES");
+        else System.out.print("NO");
+    }
+}
