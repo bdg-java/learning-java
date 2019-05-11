492b03607e42cb66f24fa72698534c4930ba5b5b
Armen Melkumyan <armmelkumian@gmail.com>
2019-05-11 10:47:50 +0400

    21-24

--- src/com/bdg/App_24.java
+++ src/com/bdg/App_24.java
@@ -5,19 +5,19 @@ public class App_24 {
 
     public static void main(String[] args) {
         int a = 2;
-        int b = 4;
+        int b = 3;
         int c = 2;
         boolean isEqual = false;
         //Ev i nshanna
 
         //nshanakuma a ev b havasar e 2-i
-        if (a == 2 && b == 2) {
+        if (a == 1 ) {
             isEqual = true;
         }
-        if (a == 2 && c == 2) {
+        if (a == 1) {
             isEqual = true;
         }
-        if (b == 2 && c == 2) {
+        if (b == 1) {
             isEqual = true;
         }
         if (isEqual) {
