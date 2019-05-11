de900110087774b1608949f6065a365aebfcf233
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-11 18:02:04 +0400

    Initial commit

--- src/com/bdg/App_24.java
+++ src/com/bdg/App_24.java
@@ -4,26 +4,18 @@ public class App_24 {
 
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        int c = 2;
-        boolean isEqual = false;
-        //Ev i nshanna
 
-        //nshanakuma a ev b havasar e 2-i
-        if (a == 1 ) {
-            isEqual = true;
-        }
-        if (a == 1) {
-            isEqual = true;
-        }
-        if (b == 1) {
-            isEqual = true;
-        }
-        if (isEqual) {
-            System.out.println("TRUE");
+        int a = 2;
+        int b = 2;
+        int c = 10;
+
+        if (( a == 2 && b == 2 ) || ( a == 2 && c == 2) || ( b == 2 && c == 2){
+            System.out.println("true");
+
         } else {
-            System.out.println("FALSE");
+
+            System.out.println("false");
+
+
         }
-    }
 }
