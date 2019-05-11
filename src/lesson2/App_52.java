9f86c0780daa415419be60f5789e0a03ae6ad039
AsArabyan <ahhn-93@mail.ru>
2019-05-11 23:15:10 +0400

    App_52.java

---
+++ src/lesson2/App_52.java
@@ -0,0 +1,18 @@
+package lesson2;
+
+public class App_52 {
+    public static void main(String[] args) {
+        boolean t = false;
+        int a = 333;
+        int x = a / 100;
+        int y = a / 10 % 10;
+        int z = a % 10;
+
+        if (x == y || y == z || x == z) {
+            t = true;
+        } else {
+            t = false;
+            System.out.print(t);
+        }
+    }
+}
