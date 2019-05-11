7b22f6fa937ba4d3009a2397ec20898647903a4e
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-11 17:40:50 +0400

    Initial commit

---
+++ src/com/bdg/App_23.java
@@ -0,0 +1,19 @@
+package com.bdg;
+
+public class App_23 {
+    public static void main (String[] args) {
+
+        int a = 38;
+        int b = 29;
+        int c = 40;
+
+        if (a ==1 || b == 1 || c == 1) {
+            System.out.println("true")
+
+        } else {
+            System.out.print("false")
+        }
+
+
+    }
+}
