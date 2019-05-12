d40753ec39f4a95dd9881b337d8dc72ee82d5553
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 18:46:01 +0400

    Task 159 done

--- src/com/lesson3/Task159.java
+++ src/com/lesson3/Task159.java
@@ -2,7 +2,7 @@ package com.lesson3;
 
 public class Task159 {
     public static void main(String[] args) {
-        long mul = 1;
+        double mul = 1;
         for (int i = 100; i <= 999; i++)
             if (i % 3 == 1 && i % 4 == 2) {
                 mul *= i;
