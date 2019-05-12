d59d983ce1384fd84bf91e9a588022ee7eee18c2
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 18:05:44 +0400

    Task 152 done

---
+++ src/com/lesson3/Task152.java
@@ -0,0 +1,13 @@
+package com.lesson3;
+
+public class Task152 {
+    public static void main(String[] args) {
+
+        int n = 56, mul = 1;
+        for (int i = 1; i <= 999999; i++) {
+            if (n % i == 0)
+                mul *= i;
+        }
+        System.out.println(mul);
+    }
+}
