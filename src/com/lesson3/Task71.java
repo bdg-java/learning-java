43f9bdc66b4e197bec934846169150faebbdd758
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 11:40:04 +0400

    Task 71 done

---
+++ src/com/lesson3/Task71.java
@@ -0,0 +1,12 @@
+package com.lesson3;
+
+public class Task71 {
+    public static void main(String[] args) {
+        double x = 2.4, y = 0;
+        while (x < 7.6) {
+            y = Math.tan(2 * x + x * x);
+            x = x + 0.2;
+        }
+        System.out.println(y);
+    }
+}
