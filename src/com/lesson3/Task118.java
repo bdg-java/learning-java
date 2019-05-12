ca7287d43b4a9eeb5ccae8b1e124232f68935ec3
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 13:13:43 +0400

    Task 118 done

---
+++ src/com/lesson3/Task118.java
@@ -0,0 +1,12 @@
+package com.lesson3;
+
+public class Task118 {
+    public static void main(String[] args) {
+        double sum = 0;
+        int x = 3;
+        for (int n = 0; n < 7; n++) {
+            sum += Math.abs(Math.pow((2 * x + n), n)) / ((n + 1) * (n + 2));
+        }
+        System.out.println(sum);
+    }
+}
