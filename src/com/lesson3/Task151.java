70b6d543a3c9118deee68312d626251c6496a9a1
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 18:12:51 +0400

    Task 151 done

--- src/com/lesson3/Task151.java
+++ src/com/lesson3/Task151.java
@@ -2,8 +2,8 @@ package com.lesson3;
 
 public class Task151 {
     public static void main(String[] args) {
-        int n = 121, sum = 0;
-        for (int i = 1; i <= 99999; i++) {
+        int n = 112, sum = 0;
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0)
                 sum += i;
         }
