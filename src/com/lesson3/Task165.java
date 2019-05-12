50a7cc1ca8174aae0e4c72b68d1442e5b1d5dfaf
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-12 19:59:38 +0400

    Task 165 done

--- src/com/lesson3/Task165.java
+++ src/com/lesson3/Task165.java
@@ -4,7 +4,8 @@ public class Task165 {
     public static void main(String[] args) {
         boolean t = false;
         int n = 81;
-        for (int i = 1; i < Math.sqrt(n); i++) {
+        double s = Math.sqrt(n);
+        for (int i = 1; i <= s; i++) {
             if (n == Math.pow(3, i)) {
                 t = true;
                 break;
