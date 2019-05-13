4285234ed532a727721767edcd383f004faedf55
AsArabyan <ahhn-93@mail.ru>
2019-05-13 22:10:55 +0400

    App_72.java

---
+++ src/lesson3/App_84.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+public class App_84 {
+    public static void main(String[] args) {
+        double y = 0.0;
+        double x = -6.4;
+        while (x < -2.4) {
+            x = (double) Math.round(x*100) / 100;
+            y += Math.exp(Math.cos(x)/ Math.sin(x));
+            x = x + 0.2;
+            System.out.println(y);
+
+        }
+
+    }
+}
