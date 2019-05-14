e5ca4a3f7aaf05b6b4fbbb17ea56e9f2d2164aaa
AsArabyan <ahhn-93@mail.ru>
2019-05-14 21:49:48 +0400

    App_176.java

---
+++ src/lesson3/App_176.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class App_176 {
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 1, n = 14;
+        System.out.println("1 : " + n1 + "\n" + "2 : " + n2);
+        for (int i = 3; i <= n; i++) {
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.println(i + " : " + n3);
+        }
+    }
+}
