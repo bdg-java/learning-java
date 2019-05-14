f5852bc752a940e00eaa6b23d0a4b28c231f0b63
AsArabyan <ahhn-93@mail.ru>
2019-05-14 21:23:39 +0400

    App_174.java

---
+++ src/lesson3/App_174.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class App_174 {
+    public static void main(String[] args) {
+        double x = 2;
+        int n = 14;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(x);
+            x = 2 + 1 / x;
+        }
+    }
+
+    }
