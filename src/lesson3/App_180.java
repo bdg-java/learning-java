9010fa8e0cce3cb2e22f998ca934e753472562f7
AsArabyan <ahhn-93@mail.ru>
2019-05-14 22:42:42 +0400

    App_180.java

---
+++ src/lesson3/App_180.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class App_180 {
+    public static void main(String[] args) {
+        int n = 243;
+        double a = Math.sqrt(n);
+            if (n == Math.pow(3, a)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+
+            }
+    }
+}
