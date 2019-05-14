2b5ff6811a6556bce048eeb8cbfc2679e4be03cc
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-15 01:00:36 +0400

    Task 182 done

---
+++ src/com/lesson3/Task182.java
@@ -0,0 +1,10 @@
+package com.lesson3;
+
+public class Task182 {
+    public static void main(String[] args) {
+        int n = 51, i = 1;
+        for (i = 1; i * i <= n; i++) ;
+        System.out.println(--i);
+
+    }
+}
