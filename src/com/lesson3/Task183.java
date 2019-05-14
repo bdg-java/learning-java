7bc8c2660e56baecf262a8c0814ab4b3bd7fab73
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-15 01:15:17 +0400

    Task 183 done

---
+++ src/com/lesson3/Task183.java
@@ -0,0 +1,9 @@
+package com.lesson3;
+
+public class Task183 {
+    public static void main(String[] args) {
+        int n = 65, i;
+        for (i = 1; Math.pow(3, i) <= n; i++) ;
+        System.out.println(i);
+    }
+}
