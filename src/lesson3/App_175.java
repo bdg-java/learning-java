9ac29fd84a2e2cc671516d41010ca6ebc840ea77
AsArabyan <ahhn-93@mail.ru>
2019-05-14 21:32:54 +0400

    App_175.java

---
+++ src/lesson3/App_175.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+public class App_175 {
+    public static void main(String[] args) {
+        double x = 1;
+        int n = 14;
+        for (int k = 1; k <= n; k++) {
+            System.out.println(x);
+            x = (x + 1) / k;
+        }
+    }
+
+}
+
+
+
