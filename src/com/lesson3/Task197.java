7563953e63f1d755b3ea52c3a514a10c96093341
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-15 15:43:49 +0400

    Task 197 done

---
+++ src/com/lesson3/Task197.java
@@ -0,0 +1,14 @@
+package com.lesson3;
+
+public class Task197 {
+    public static void main(String[] args) {
+        int n = 4, t = 1, x = 2;
+        double sum = 1;
+        int q = 2 * n;
+        for (int i = 2; i <= q; i += 2) {
+            t = i * (i - 1) * t;
+            sum += Math.pow(-1, i / 2) * Math.pow(x, i) / t;
+        }
+        System.out.println(sum);
+    }
+}
