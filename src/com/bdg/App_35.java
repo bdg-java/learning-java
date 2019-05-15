2d1d84271865c597029ea62631a0e6adb11a2c0e
Karo Vardevaryan <vardevaryan.karo@gmail.com>
2019-05-15 15:46:00 +0400

    App_35

---
+++ src/com/bdg/App_35.java
@@ -0,0 +1,16 @@
+package com.bdg;
+
+public class App_35 {
+    public static void main(String []args){
+        int a = 3;
+        int b = 5;
+        int c = 7;
+        int d = 12;
+
+        if ((a == b + c + d)|| (b == a + c + d) || (c == a + b + d) || (d == a + b + c)){
+            System.out.println("TRUE");}
+            else {
+                System.out.println ("FALSE");
+        }
+    }
+}
