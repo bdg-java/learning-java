24f13080a98f598783e6c9de7991f63b08957ed2
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-15 15:40:15 +0400

    Task 196 done

--- src/com/lesson3/Task196.java
+++ src/com/lesson3/Task196.java
@@ -4,7 +4,8 @@ public class Task196 {
     public static void main(String[] args) {
         int n = 6, t = 1, x = 2;
         double sum = x;
-        for (int i = 3; i <= n; i += 2) {
+        int q = 2 * n + 1;
+        for (int i = 3; i <= q; i += 2) {
             t = i * (i - 1) * t;
             sum += Math.pow(-1, (i - 1) / 2) * Math.pow(x, i) / t;
         }
