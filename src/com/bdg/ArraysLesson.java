4d727f93c8f2cee18b150280dfd3de5208b6711e
araqs_sh <araqs.shahbazian@gmail.com>
2019-05-15 21:37:22 +0400

    Arrays

---
+++ src/com/bdg/ArraysLesson.java
@@ -0,0 +1,19 @@
+package com.bdg;
+
+public class ArraysLesson {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 9;
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        IntegerArrayOperations.displayArray(array);
+
+        int sum = 0;
+        int cout = 0;
+        for (int i = a; i <= b; i++) {
+            sum = sum + array[i];
+            cout = cout + 1;
+        }
+        System.out.println((double) sum / cout);
+    }
+}
