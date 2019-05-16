cc5913b0686906597d7b022b6895a5f8d1d12d04
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-16 20:56:14 +0400

    Task 212 done

---
+++ src/com/lesson4/Task212.java
@@ -0,0 +1,17 @@
+package com.lesson4;
+
+
+public class Task212 {
+    public static void main(String[] args) {
+        double sum = 0;
+        int[] a = IntegerArrayoperations.getDefaultArray();
+        int i = 0;
+        for (i = 0; i < a.length; i++) {
+            sum += a[i] * a[i];
+        }
+        sum = Math.sqrt(sum / i);
+        System.out.println(sum);
+
+
+    }
+}
