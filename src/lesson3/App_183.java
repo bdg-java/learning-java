02dda3a1c6bf7472c3d66e735a7e297b7612bd96
AsArabyan <ahhn-93@mail.ru>
2019-05-16 22:11:02 +0400

    App_183.java

---
+++ src/lesson3/App_183.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class App_183 {
+    public static void main(String[] args) {
+        int n = 45;
+
+        for(int i = 1; Math.pow(3, i)<= n; i++)
+        System.out.println(i);
+
+    }
+}
