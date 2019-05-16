29cb78a87f6377bceadd75a48eff6aa944da4d39
AsArabyan <ahhn-93@mail.ru>
2019-05-16 23:21:41 +0400

    App_201.java

---
+++ src/lesson3/App_201.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+public class App_201 {
+    public static void main(String[] args) {
+        int a = 356;
+        int b = 0;
+
+        while (a > 0) {
+            a = a / 10;
+            b++;
+
+        }
+        System.out.println(b);
+
+    }
+}
