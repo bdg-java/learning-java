ff32598ff1e4a9c8fb7d7d610ebee458549f3665
AsArabyan <ahhn-93@mail.ru>
2019-05-16 21:59:50 +0400

    App_182.java

---
+++ src/lesson3/App_182.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class App_182 {
+    public static void main(String[] args) {
+        int n = 45;
+        int k = 4;
+        for(int i = 1; i<=n; i++) {
+            System.out.println(--k);
+        }
+
+    }
+}
