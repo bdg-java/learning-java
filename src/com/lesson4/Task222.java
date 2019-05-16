b3d6a3bafbaaf23c594f6c208339e3d5411a2acd
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-16 21:37:48 +0400

    Task 222 done

---
+++ src/com/lesson4/Task222.java
@@ -0,0 +1,15 @@
+package com.lesson4;
+
+public class Task222 {
+    public static void main(String[] args) {
+        int c = 0;
+        int a = 2, b = 4;
+        int[] arr = IntegerArrayoperations.getDefaultArray();
+        if (a >= 0 && b <= arr.length - 1) {
+            for (int i = a + 1; i < b; i++) {
+                c++;
+            }
+            System.out.println(c);
+        }
+    }
+}
