c53152529f0c9a21deef1da7c7d21467084fb390
AGharibyan <anigharib@gmail.com>
2019-05-17 04:46:36 +0500

    App_211_214

---
+++ src/com/bdg/agharibyan/app_211_260/App_212.java
@@ -0,0 +1,25 @@
+package com.bdg.agharibyan.app_211_260;
+
+/**
+ * @author Ani Gharibyan
+ */
+import com.bdg.agharibyan.IntegerArrayOperations;
+
+public class App_212 {
+    public static void main(String[] args) {
+
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        IntegerArrayOperations.displayArray(array);
+
+        int sum = 0;
+        int cout = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                sum += Math.pow(array[i], 2);
+                cout++;
+            }
+        }
+        System.out.println(Math.sqrt(sum / cout));
+    }
+}
