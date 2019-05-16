0101350a972a14b54d6d5fc25fdf18fb5c15c3e2
AsArabyan <ahhn-93@mail.ru>
2019-05-16 23:34:46 +0400

    App_207.java

---
+++ src/lesson3/App_207.java
@@ -0,0 +1,18 @@
+package lesson3;
+
+public class App_207 {
+    public static void main(String[] args) {
+        int a = 246;
+        boolean t = false;
+
+        while (a > 0) {
+            if (a % 10 == 2) {
+                t = true;
+                break;
+            }
+            a = a / 10;
+        }
+        System.out.println(t);
+
+    }
+}
