2bd19bc39d99dccd13a11300acdd021c19a61b0e
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-16 23:43:11 +0400

    Task 219 done

--- src/com/lesson4/Task219.java
+++ src/com/lesson4/Task219.java
@@ -3,11 +3,12 @@ package com.lesson4;
 public class Task219 {
     public static void main(String[] args) {
         int c = 0;
-        int k = 10;
+        int k = 3;
         int[] a = IntegerArrayoperations.getDefaultArray();
         for (int i = 1; i < a.length; i++) {
-            if (k % i == 0)
+            if (i % k == 0) {
                 c++;
+            }
         }
         System.out.println(c);
     }
