50264b7e427afc8b90279a8833680404e8f2de44
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-16 20:58:51 +0400

    Task 212 done

--- src/com/lesson4/Task212.java
+++ src/com/lesson4/Task212.java
@@ -7,7 +7,8 @@ public class Task212 {
         int[] a = IntegerArrayoperations.getDefaultArray();
         int i = 0;
         for (i = 0; i < a.length; i++) {
-            sum += a[i] * a[i];
+            if (a[i] > 0)
+                sum += a[i] * a[i];
         }
         sum = Math.sqrt(sum / i);
         System.out.println(sum);
