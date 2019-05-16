0d952466881a783166f6acd73c6863b4f5259cd4
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-16 21:08:00 +0400

    Task 215 done

--- src/com/lesson4/Task215.java
+++ src/com/lesson4/Task215.java
@@ -5,8 +5,7 @@ public class Task215 {
         double sum = 0;
         int[] a = IntegerArrayoperations.getDefaultArray();
         int i = 0;
-        for (i = 0; i < a.length; i++) {
-            if ((i & 1) == 0)
+        for (i = 0; i < a.length; i += 2) {
                 sum += a[i];
         }
         System.out.println(sum);
