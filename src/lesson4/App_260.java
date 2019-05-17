cce35cc84789f31e9edd2f54561cc8df161492fc
AsArabyan <ahhn-93@mail.ru>
2019-05-18 03:20:46 +0400

    App_260.java

---
+++ src/lesson4/App_260.java
@@ -0,0 +1,17 @@
+package lesson4;
+
+public class App_260 {
+    public static void main(String[] args) {
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+    }
+}
