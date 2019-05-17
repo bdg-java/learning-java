02b13f93405971395c868779c85c657ef4ffb85a
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-17 22:17:25 +0400

    Task 253 done

---
+++ src/com/lesson4/Task253.java
@@ -0,0 +1,16 @@
+package com.lesson4;
+
+public class Task253 {
+    public static void main(String[] args) {
+        int[] a = IntegerArrayoperations.getDefaultArray();
+        int min = a[0];
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min)
+                min = a[i];
+            if (a[i] > max)
+                max = a[i];
+        }
+        System.out.println(max + min);
+    }
+}
