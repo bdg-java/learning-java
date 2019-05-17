7c5aa03a601eef2df04daab223aaa407d7b63575
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-17 21:59:57 +0400

    Initial commit

---
+++ src/com/bdg/App_166.java
@@ -0,0 +1,19 @@
+package com.bdg;
+
+public class App_166 {
+    public static void main(String[] args) {
+
+        Boolean t = false;
+        int n = 16;
+
+        for ( int i = 1; i <= 100; i++){
+            if ( n == Math.pow(4,i)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+
+            }
+        }
+
+    }
+}
