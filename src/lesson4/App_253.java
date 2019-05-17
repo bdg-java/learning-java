900affeb409c6e4773af5531dcd1c3810fb14300
AsArabyan <ahhn-93@mail.ru>
2019-05-18 02:51:37 +0400

    App_253.java

---
+++ src/lesson4/App_253.java
@@ -0,0 +1,16 @@
+package lesson4;
+
+public class App_253 {
+    public static void main(String[] args) {
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+            if (array[i] > max)
+                max = array[i];
+        }
+        System.out.println(max + min);
+    }
+}
