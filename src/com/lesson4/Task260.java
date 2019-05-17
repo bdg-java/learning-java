1b4565329001ea25950b13471463c4cc84c39e82
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-18 01:12:42 +0400

    Task 260 done

--- src/com/lesson4/Task260.java
+++ src/com/lesson4/Task260.java
@@ -1,15 +1,14 @@
 package com.lesson4;
 
-public class Task258 {
+public class Task260 {
     public static void main(String[] args) {
         int[] a = IntegerArrayoperations.getDefaultArray();
-        int max = IntegerArrayoperations.getMaxElementOfArray(a);
+        int min = IntegerArrayoperations.getMinElementOfArray(a);
         int i;
         for (i = a.length - 1; i >= 0; i--) {
-            if (a[i] == max)
+            if (a[i] == min)
                 break;
         }
         System.out.println(i);
     }
-
 }
