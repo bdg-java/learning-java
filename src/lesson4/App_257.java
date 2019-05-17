4f12ea12eef106d2f6a43f3f9eea46c77dbb5ba5
AsArabyan <ahhn-93@mail.ru>
2019-05-18 03:10:09 +0400

    App_257.java

---
+++ src/lesson4/App_257.java
@@ -0,0 +1,17 @@
+package lesson4;
+
+public class App_257 {
+    public static void main(String[] args) {
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        int max = array[0];
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+
+    }
+}
