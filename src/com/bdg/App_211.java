396b319dc328dc039821903f8fb2cf709fca2559
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-18 00:18:26 +0400

    Initial commit

---
+++ src/com/bdg/App_211.java
@@ -0,0 +1,34 @@
+package com.bdg;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class App_211 {
+    public static void main(String[] args) {
+
+
+        int[] arr = {10, 23, 45, -21, 57, 49, 98, 8, -28, 76};
+        System.out.println(arr.length);
+
+        int sum = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) ;
+
+            sum += arr[i];
+
+
+
+            System.out.println( sum / arr.length);
+
+
+        }
+
+    }
+
+}
+
+
+
+
---
+++ src/com/bdg/testing.java
@@ -0,0 +1,22 @@
+package com.bdg;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class testing {
+
+
+        public static void main(String[] args) {
+            int [] arr = {10 ,35, 7,0,1,12,98,4,8,4,0,9};
+            System.out.println(arr.length);
+            int sum = 0;
+            for (int i = 0; i < arr.length ; i++) {
+                sum += arr[i];
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            System.out.println("The arithmetical average is " + (double)sum/arr.length);
+
+        }
+
+    }
