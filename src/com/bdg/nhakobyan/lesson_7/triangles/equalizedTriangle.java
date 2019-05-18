8fba9f1e56e5894a5179f967ac93dad072050955
nairihakobyan <nairihakobyan992@gmail.com>
2019-05-18 22:56:37 +0400

    Triangles are done

---
+++ src/com/bdg/nhakobyan/lesson_7/triangles/equalizedTriangle.java
@@ -0,0 +1,23 @@
+package com.bdg.nhakobyan.lesson_7.triangles;
+
+public class equalizedTriangle {
+    public static void main(String[] args) {
+        int [][]  triangle = new int[4][4];
+        int starQuantity = 1;
+        int space = triangle.length;
+        for (int i = 0; i < triangle.length ; i++) {
+            if (i > 0){
+                starQuantity++;
+                space--;
+            }
+            for (int k = 0; k < space ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < starQuantity ; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+        }
+    }
+}
