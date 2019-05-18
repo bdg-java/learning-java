dad09f1913949c57511ad178d28e12554fda4f07
AGharibyan <anigharib@gmail.com>
2019-05-18 10:46:52 +0500

    App_257_260

--- src/com/bdg/agharibyan/ArraysLesson.java
+++ src/com/bdg/agharibyan/ArraysLesson.java
@@ -1,7 +1,9 @@
 package com.bdg.agharibyan;
 
 public class ArraysLesson {
-
+    /**
+     * @author Ani Gharibyan
+     */
     public static void main(String[] args) {
         int a = 2;
         int b = 9;
