7d3ec1df0b8f27deff21f412f2a25c2dba168a2f
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-19 18:41:04 +0400

    Task 295 done

--- src/com/lesson5/Task295.java
+++ src/com/lesson5/Task295.java
@@ -9,7 +9,7 @@ public class Task295 {
         int[] a = IntegerArrayoperations.getDefaultArray();
         int j = 0;
         int c = 0;
-        for (int i = 1; i <= a.length; i += 2) {
+        for (int i = 1; i < a.length; i += 2) {
             c++;
         }
         int[] b = new int[c];
