631db54738e7340db2075ad72bed7dd59c36efb9
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-19 17:46:29 +0400

    Task 291 done

---
+++ src/com/lesson5/Task291.java
@@ -0,0 +1,28 @@
+package com.lesson5;
+
+import com.lesson4.IntegerArrayoperations;
+
+import java.util.Arrays;
+
+public class Task291 {
+    public static void main(String[] args) {
+        int[] a = IntegerArrayoperations.getDefaultArray();
+
+        int j = 0;
+        int c = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] <= 0) {
+                c++;
+            }
+        }
+        int[] b = new int[c];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] <= 0) {
+                b[j] = a[i];
+                j++;
+            }
+
+        }
+        System.out.println(Arrays.toString(b));
+    }
+}
