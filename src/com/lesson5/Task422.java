e71bf9eb56a0420bd5ac955ccf79862c3ac245f7
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-19 19:39:45 +0400

    Task 422 done

---
+++ src/com/lesson5/Task422.java
@@ -0,0 +1,19 @@
+package com.lesson5;
+
+public class Task422 {
+    public static void main(String[] args) {
+        int[][] m = MatrixDefaultDeclaration.GetDefaultMatrix();
+        int c = 0;
+        int sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (m[i][j] % 5 == 0) {
+                    c++;
+                    sum += m[i][i];
+                }
+            }
+        }
+        System.out.println(sum / c);
+    }
+
+}
