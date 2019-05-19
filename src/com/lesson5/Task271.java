f00218325ee5a67080f7cd985c9470e2b666f801
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-19 13:58:23 +0400

    Task 271 done

--- src/com/lesson5/Task271.java
+++ src/com/lesson5/Task271.java
@@ -4,8 +4,7 @@ public class Task271 {
     public static void main(String[] args) {
         int c = 0;
         char[] a = CharArrayDeclaration.getDef_array();
-        int n = a.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < a.length; i++) {
             if (a[i] == 'a')
                 c++;
         }
