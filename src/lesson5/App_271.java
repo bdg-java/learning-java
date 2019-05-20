6eedafadb43482d48b80797c6d2fcbef21841417
AsArabyan <ahhn-93@mail.ru>
2019-05-20 21:53:15 +0400

    App_271

---
+++ src/lesson5/App_271.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class App_271 {
+    public static void main(String[] args) {
+        int count = 0;
+        char [] array = {'a', 'b', 'c'};
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == 'a');
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
