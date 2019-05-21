cb4b3b2c2dc595baa063284de407327558dd1656
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-21 21:06:25 +0400

    RectangleLeftBottom

---
+++ src/com/lesson5/TaskRectangleLeftBottom.java
@@ -0,0 +1,15 @@
+package com.lesson5;
+
+public class TaskRectangleLeftBottom {
+    public static void main(String[] args) {
+        int n = 10;
+        char[][] m = CharStarMatrixArrayDeclaration.getDefaultMatrix();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
