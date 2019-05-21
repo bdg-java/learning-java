b7da88a56eae2c0c8c2e6bd194a687ce0febb17b
AsArabyan <ahhn-93@mail.ru>
2019-05-22 00:21:01 +0400

    App_299

---
+++ src/lesson5/App_299.java
@@ -0,0 +1,32 @@
+package lesson5;
+
+import lesson4.IntegerArrayOperations;
+
+import java.util.Arrays;
+
+public class App_299 {
+    public static void main(String[] args) {
+        int[] array = IntegerArrayOperations.getDefaultArray();
+        int x = 0;
+        int count = 0;
+        int[] newarray = new int[array.length - 0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                newarray[x] = array[i];
+                x++;
+            }
+        }
+
+        System.out.println(Arrays.toString(newarray));
+    }
+    private static int getCount(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
