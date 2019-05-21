8e3ff62eb85b75f8c2e8927b19cda91874a51200
AsArabyan <ahhn-93@mail.ru>
2019-05-21 23:34:37 +0400

    App_295

---
+++ src/lesson5/App_295.java
@@ -0,0 +1,24 @@
+package lesson5;
+
+import lesson4.IntegerArrayOperations;
+
+import java.util.Arrays;
+
+public class App_295 {
+        public static void main(String[] args) {
+            int[] array = IntegerArrayOperations.getDefaultArray();
+            int x = 0;
+
+            for (int i = 1; i < array.length; i += 2) {
+                x++;
+            }
+            int[] newarray = new int[x];
+            for (int i = 1; i < array.length; i += 2) {
+                newarray[x] = array[i];
+                x++;
+            }
+            System.out.println(Arrays.toString(newarray));
+
+        }
+    }
+
