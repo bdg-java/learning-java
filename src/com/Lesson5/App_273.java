a62ba91614e8e699c6e658469aec2efb1adc8b1f
Tamara Minasyan <minatama@gmail.com>
2019-05-21 16:53:54 +0400

    Initial commit

---
+++ src/com/Lesson5/App_273.java
@@ -0,0 +1,15 @@
+package com.Lesson5;
+
+public class App_273 {
+    public static void main(String[] args) {
+        int n=10;
+        int k=0;
+        char[] a = {'s', 'b', 'c','k','j','b', 's', '4','s','q'};
+        for(int i=0 ; i<a.length; i++){
+            if (a[i]=='s')
+                k++;
+
+        }
+        System.out.println(k);
+    }
+}
