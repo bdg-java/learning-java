531f4af406d72374e61b670af307194161e25230
Tamara Minasyan <minatama@gmail.com>
2019-05-22 03:52:31 +0400

    Initial commit

---
+++ src/com/Lesson5/App_278.java
@@ -0,0 +1,24 @@
+package com.Lesson5;
+
+public class App_278 {
+    public static void main(String[] args) {
+        int n = 10;
+        int c = 0;
+        int k = 0;
+
+        char[] a = {'d', 'b', 'c', 'k', 'j', 'b', 'r', '4', 'h', 'q'};
+        char [] b = new char [a.length] ;
+        for (int i = 0; i < a.length; i=i+2) {
+
+           // if (i/2==0) {
+                b[k] = a[i];
+                k++;
+
+            }
+
+        System.out.println (b) ;
+        }
+
+
+    }
+
