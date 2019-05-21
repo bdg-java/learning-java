8859ee1e5bb1a55def98d66cae15a7cc02b2f440
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-22 00:09:32 +0400

    Initial commit

--- src/com/bdg/App_273.java
+++ src/com/bdg/App_273.java
@@ -4,16 +4,25 @@ public class App_273 {
 
     public static void main(String[] args) {
 
-        int n = 4;
 
         char [] text = { 'a', 's' , 't' , 's' };
 
         int c = 0;
+        int sum = 0;
+
 
 
         for ( int i = 0; i < text.length; i++){
 
         if ( text[i] == 's') {
+
+            c++;
+
+            sum += i;
+
+
+
+            System.out.println(sum);
 
 
         }
