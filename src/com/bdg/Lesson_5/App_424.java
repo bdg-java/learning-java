01a69a5decbdcf6c19bf56d3aaa63ae09bd18966
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-05-21 21:36:15 +0400

    421-430

---
+++ src/com/bdg/Lesson_5/App_424.java
@@ -0,0 +1,22 @@
+package com.bdg.Lesson_5;
+
+public class App_424 {
+    public static void main(String[] args) {
+        int[][] matrix = {{ 5, 4, 31, 18},
+                { 12, -5 , -6 , 11},
+                { 23, 4, 56 ,14},
+                { 10, 32, -9, 7}
+        };
+        int x = 0;
+        double sum = 0;
+
+        for (int i = 0; i< matrix.length; i++){
+            for (int j = 0; j <= i; j++){
+                if (matrix[i][j] % 2 !=0);
+                x++;
+                sum +=matrix[i][j];
+            }
+        }
+        System.out.println(sum/x);
+    }
+}
