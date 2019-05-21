d5e680b49a9d4c5ad12eb3cd581cdf9ea4675e71
Tamara Minasyan <minatama@gmail.com>
2019-05-21 16:35:18 +0400

    Initial commit

--- src/com/Lesson5/App_272.java
+++ src/com/Lesson5/App_272.java
@@ -2,14 +2,14 @@ package com.Lesson5;
 
 public class App_272 {
     public static void main(String[] args) {
-        int n=24;
+        int n=6;
         int k=0;
         boolean t = false;
-        char[] a = {'b', 'b', 'c','a','k','j','b'};
+        char[] a = {'b', 'b', 'c','k','j','b'};
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 'b')
                 k++;
-            if (k >= n / 2) {
+            if (k >= n/ 2) {
                 t = true;
                 break;
             }
