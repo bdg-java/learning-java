68d8700c76f5ad605e93c06ff97dfb046f2424eb
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-21 20:32:50 +0400

    some review

--- src/com/lesson5/MatrixDefaultDeclaration.java
+++ src/com/lesson5/MatrixDefaultDeclaration.java
@@ -1,12 +1,13 @@
 package com.lesson5;
 
 public class MatrixDefaultDeclaration {
-    private static final int[][] def_matrix = {{5, -5, 2, 0},
+    private static final int[][] def_matrix = {{5, -2, 2, 0},
             {9, 6, -89, 2},
             {0, -5, 6, 4},
             {1, 2, 8, 16}
     };
 
+    //according java method convention method names should be male case :)
     public static int[][] GetDefaultMatrix() {
         return def_matrix;
     }
