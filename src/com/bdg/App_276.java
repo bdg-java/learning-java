7bed8c2dd189f57974f07fe610c3e381b1eb3f11
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-05-22 00:25:52 +0400

    Initial commit

---
+++ src/com/bdg/App_276.java
@@ -0,0 +1,30 @@
+package com.bdg;
+
+public class App_276 {
+
+    public static void main(String[] args) {
+
+
+        Boolean t = false;
+
+
+        char[] text = {'a', 'r', 't', '3', 'b', 'c', 'b', 'd' };
+
+        int n = 0;
+
+        int c = 0;
+
+
+        for (int i = 0; i < text.length; i++) {
+
+            if (text[i] == 'r') {
+
+                c++;
+
+                System.out.println(true);
+
+
+            }
+        }
+    }
+}
