04e306d99ab19004db95de14c210c78933f8057b
Tamara Minasyan <minatama@gmail.com>
2019-05-22 02:37:19 +0400

    Initial commit

---
+++ src/com/Lesson5/App_276.java
@@ -0,0 +1,31 @@
+package com.Lesson5;
+
+public class App_276 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        int c = 0;
+
+        char[] a = {'s', 'b', 'c', 'k', 'j', 'b', 'r', '4', 'h', 'q'};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 'r') {
+                c++;
+                boolean t = true;
+            } else {
+                boolean t = false;
+            }
+            System.out.println(t);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
