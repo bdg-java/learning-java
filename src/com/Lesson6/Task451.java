2a26b5d9e655463831a8ed662cda654d215eb940
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-23 21:08:55 +0400

    Task451

--- src/com/Lesson6/Task451.java
+++ src/com/Lesson6/Task451.java
@@ -2,15 +2,20 @@ package com.Lesson6;
 
 import com.lesson5.MatrixDefaultDeclaration;
 
+import java.util.Arrays;
+
 public class Task451 {
     public static void main(String[] args) {
         int[][] m = MatrixDefaultDeclaration.GetDefaultMatrix();
         int n = m.length;
-        int[] b = new int[n];
-        //for(int i = 0; i < n ; i++){
-        // for(int j = 0; j < n; )
-        //}
-
-
+        double[] b = new double[n];
+        for (int i = 0; i < n; i++) {
+            if (i != n - i - 1) {
+                b[i] = (double) (m[i][i] + m[i][n - 1 - i]) / 2;
+            } else {
+                b[i] = (double) m[i][i];
+            }
+        }
+        System.out.println(Arrays.toString(b));
     }
 }
