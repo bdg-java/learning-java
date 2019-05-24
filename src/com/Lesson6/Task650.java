256831771742ec7918b0696f283e4f37ce461c75
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-24 22:16:17 +0400

    Task650

--- src/com/Lesson6/Task650.java
+++ src/com/Lesson6/Task650.java
@@ -7,13 +7,13 @@ public class Task650 {
         int c = 0;
         char[] a = CharArrayDeclaration.getDef_array();
         boolean t = true;
-        char[] b = new char[]{'a', 'b', 'z', 'b', 'a', 'H'};
+        char[] b = new char[]{'a', 'b', 'z', 'b', 'a', 'A', 'H'};
         for (int i = 0; i < a.length; i++) {
             for (int p = i - 1; p >= 0; p--) {
                 if (a[i] == a[p]) {
                     t = false;
                     break;
-                }
+                } else t = true;
             }
             if (t == true) {
                 for (int j = 0; j < b.length; j++) {
