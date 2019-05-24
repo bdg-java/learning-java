1917475a667ded414b9372bf0ad404721b39de66
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-24 23:55:59 +0400

    Task657

---
+++ src/com/Lesson6/Task657.java
@@ -0,0 +1,20 @@
+package com.Lesson6;
+
+import com.lesson5.CharArrayDeclaration;
+
+public class Task657 {
+    public static void main(String[] args) {
+        char[] a = CharArrayDeclaration.getDef_array();
+        int n = a.length - 1;
+        char[] b = new char[n / 2 + 1];
+        int j = 0;
+        int i = 0;
+        for (i = 0; i < n; i += 2) {
+            if (a[i] > a[i + 1])
+                b[j] = a[i];
+            else b[j] = a[i + 1];
+            j++;
+        }
+        System.out.println(b);
+    }
+}
