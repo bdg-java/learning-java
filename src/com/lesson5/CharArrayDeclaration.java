03bca71a55f88af7241d333254779a93f92267d0
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-25 00:05:31 +0400

    Task655

--- src/com/lesson5/CharArrayDeclaration.java
+++ src/com/lesson5/CharArrayDeclaration.java
@@ -1,7 +1,7 @@
 package com.lesson5;
 
 public class CharArrayDeclaration {
-    private static final char[] def_array = {'z', 'a', 'b', 'b', 'b', 'b', 'r', 'd', 'd', 'p', 's', 'A', 'H', 'f', '2', 'a', 'A', 'f', 'B', 'd', 'F', '(', '0', 'C', '9'};
+    private static final char[] def_array = {'z', 'a', 'b', 'b', 'b', 'b', 'r', 'd', 'x', 'p', 's', 'A', 'H', 'f', '2', 'a', 'A', 'x', 'B', 'd', 'F', '(', '0', 'C', '9'};
 
     public static char[] getDef_array() {
         return def_array;
