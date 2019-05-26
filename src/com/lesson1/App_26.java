7b58417c28b6e182bf3f23c076f49964e95fbd8a
Tamara Minasyan <minatama@gmail.com>
2019-05-27 00:59:34 +0400

    App_25

---
+++ src/com/lesson1/App_26.java
@@ -0,0 +1,16 @@
+package com.lesson1;
+
+public class App_26 {
+    public static void main(String[] args) {
+        int a = 54;
+        int b = 23;
+        int c = 86;
+        float t;
+        if ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0)) {
+            System.out.println("1");
+        } else {
+            System.out.println("2");
+        }
+
+    }
+}
