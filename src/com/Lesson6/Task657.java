9af3eb5bf7f15db6c40d02861bf98fdf86e41df2
mmelnichenko <mariamelnichenko23@gmail.com>
2019-05-26 11:03:43 +0400

    Task657

--- src/com/Lesson6/Task657.java
+++ src/com/Lesson6/Task657.java
@@ -5,10 +5,10 @@ import com.lesson5.CharArrayDeclaration;
 public class Task657 {
     public static void main(String[] args) {
         char[] a = CharArrayDeclaration.getDef_array();
-        int n = a.length - 1;
+        int n = a.length;
         char[] b = new char[n / 2 + 1];
         int j = 0;
-        for (int i = 0; i < n; i += 2) {
+        for (int i = 0; i < n - 1; i += 2) {
             if (a[i] > a[i + 1])
                 b[j] = a[i];
             else b[j] = a[i + 1];
