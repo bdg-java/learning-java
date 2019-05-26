228af6f6e3976bc3554de657b75965f85ad04fe7
Tamara Minasyan <minatama@gmail.com>
2019-05-26 16:07:32 +0400

    App_25

---
+++ src/com/lesson1/App_25.java
@@ -0,0 +1,15 @@
+package com.lesson1;
+
+public class App_25 {
+    public static void main(String[] args) {
+        int a=12;
+        int b=21;
+        int c=36;
+        if ((a+b>c) || (a+c>b) || (b+c>a) ){
+                System.out.println("y=1");
+            }else {
+            System.out.println("y=2");
+            }
+        }
+    }
+
