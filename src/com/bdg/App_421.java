1af3e27c7fb30d6e1f623c809aca2c7c5d1bc117
William Arustamyan <williamarustamyan@gmail.com>
2019-05-26 22:37:46 +0400

    some review

--- src/com/bdg/App_421.java
+++ src/com/bdg/App_421.java
@@ -5,25 +5,23 @@ public class App_421 {
     public static void main(String[] args) {
 
 
-        int [][] matrix = new int [][]{
+        int[][] matrix = new int[][]{
                 {0, 1, 2, 3},
-                {54, 32, 96, 39},
+                {54, 32, 96, 39}}; //<-- stex , (storaket er) u phakox } phakagicner pakasum
 
 
-
-
-         int m = 4;
+        int m = 4;
 
         int k = 3;
         int qount = 0;
 
         for (int i = 0; i < m; i++) {
 
-            for (int j = 0; j < m; j++){
+            for (int j = 0; j < m; j++) {
 
-                if (j< i ){
-                    if ( matrix [i][j] % k == 0){
-                        qount ++ ;
+                if (j < i) {
+                    if (matrix[i][j] % k == 0) {
+                        qount++;
 
                         System.out.print(qount);
 
@@ -33,8 +31,5 @@ public class App_421 {
 
         }
 
-        }
-
-
-    }
+    } //<-- stex el mi hat shat er drac }
 }
