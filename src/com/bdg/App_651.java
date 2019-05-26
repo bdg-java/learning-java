59c6eee36db170db3d54a86744a6a1f3333a94b5
William Arustamyan <williamarustamyan@workfront.com>
2019-05-26 11:48:34 +0400

    App 651

---
+++ src/com/bdg/App_651.java
@@ -0,0 +1,35 @@
+package com.bdg;
+
+
+/**
+ * @author William Arustamyan
+ */
+
+
+public class App_651 {
+
+    public static void main(String[] args) {
+
+        char[] array = {'a', '5', 'a', 'n', 'g', 'b', 'n'};
+
+        char[] noDuplicate = new char[array.length];
+
+        int k = 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            char found = array[i];
+            for (int m = 0; m < array.length; m++) {
+                if (array[i] == array[m]) {
+                    count++;
+                    found = array[m];
+                }
+            }
+            if (count == 1) {
+                noDuplicate[k] = found;
+                k++;
+            }
+            count = 0;
+        }
+        System.out.println(noDuplicate);
+    }
+}
