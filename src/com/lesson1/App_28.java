5d6fd818f0a5f97549d83ce5cdd3d454e649c22f
Tamara Minasyan <minatama@gmail.com>
2019-05-28 15:47:38 +0400

    App_28

---
+++ src/com/lesson1/App_28.java
@@ -0,0 +1,15 @@
+package com.lesson1;
+
+public class App_28 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 9;
+        int c = 27;
+        if ((b / a) == (c / b)) {
+            System.out.println("true");
+
+        } else {
+            System.out.println("false");
+        }
+    }
+}
