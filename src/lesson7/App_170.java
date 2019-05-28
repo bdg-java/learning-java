6917912db0d6fb85c76e5f4ab37c6a5fb6012b5c
AsArabyan <ahhn-93@mail.ru>
2019-05-28 23:23:11 +0400

    App_170

---
+++ src/lesson7/App_170.java
@@ -0,0 +1,17 @@
+package lesson7;
+
+public class App_170 {
+    public static void main(String[] args) {
+        int n = 1022;
+        double x = 0;
+        int a = (int) Math.sqrt(n) + 1;
+        for (int i = a; i <= a; i ++) {
+            if (Math.pow(2, i) > n) {
+                x = Math.pow(2,1);
+                break;
+            }
+        }
+        System.out.println(x);
+
+    }
+}
