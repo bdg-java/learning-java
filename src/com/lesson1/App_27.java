3d6de93c680ce0b9816a344ca2d00e65906bb76c
Tamara Minasyan <minatama@gmail.com>
2019-05-28 15:39:01 +0400

    App_27

---
+++ src/com/lesson1/App_27.java
@@ -0,0 +1,14 @@
+package com.lesson1;
+
+public class App_27 {
+    public static void main(String[] args) {
+        int a=12;
+        int b=24;
+        int c=36;
+        if ((b-a)==(c-b)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
