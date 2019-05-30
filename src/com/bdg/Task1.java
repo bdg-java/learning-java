5275fe1a46c7f583cb962007dfe194136627c867
AsArabyan <ahhn-93@mail.ru>
2019-05-31 00:02:24 +0400

    CarApplication

---
+++ src/com/bdg/Task1.java
@@ -0,0 +1,11 @@
+package com.bdg;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("i : " + 1);
+            i = i + 1;
+        }
+    }
+}
