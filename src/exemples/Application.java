cbfd6002795bbbb77227090398d01849fa61d0a6
AsArabyan <ahhn-93@mail.ru>
2019-05-30 23:48:05 +0400

    Application

---
+++ src/exemples/Application.java
@@ -0,0 +1,10 @@
+package exemples;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(10, 20);
+        Point p2 = new Point(10, 20);
+        System.out.println(p1.equals("XXX"));
+    }
+}
