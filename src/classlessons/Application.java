af196a890973ea923f8c9a10b4196d26edc51244
AsArabyan <ahhn-93@mail.ru>
2019-05-31 00:08:26 +0400

    Application

---
+++ src/classlessons/Application.java
@@ -0,0 +1,11 @@
+package classlessons;
+
+public class Application {
+    public static void main(String[] args) {
+
+        char[] password = {'3', 'y'};
+        boolean isValidPassword = Validator.validatePassword(password, User.validPasswordSize);
+
+        System.out.println(isValidPassword);
+    }
+}
