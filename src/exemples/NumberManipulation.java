6cd4e4b283cdc0b9e9f06d6e8cf1caa54fcd9156
AsArabyan <ahhn-93@mail.ru>
2019-05-30 23:43:21 +0400

    NumberManipulation

---
+++ src/exemples/NumberManipulation.java
@@ -0,0 +1,15 @@
+package exemples;
+
+public class NumberManipulation {
+    public static int miavor(int number) {
+        return number % 10;
+    }
+
+    public static int tasnavor(int number) {
+        return (number / 10) % 10;
+    }
+
+    public static int haryuravor(int number) {
+        return number / 100;
+    }
+}
