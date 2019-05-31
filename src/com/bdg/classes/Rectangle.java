e0d07b3cf0a428437276a1b54a9e21a9b3c250ca
nairihakobyan <nairihakobyan992@gmail.com>
2019-05-31 21:20:00 +0400

    class Rectangle

---
+++ src/com/bdg/classes/Rectangle.java
@@ -0,0 +1,46 @@
+package com.bdg.classes;
+
+public class Rectangle {
+    private float length = 1.0f;
+    private float width = 1.0f;
+    public Rectangle(){
+
+    }
+    public Rectangle(float width , float length){
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public double getArea(){
+        return getWidth()*getWidth();
+    }
+
+
+    public double getPerimeter(){
+        return 2*(getWidth()+getWidth());
+    }
+
+    public String toString(){
+        return "Rectangle[ length = "+getLength()+" ,width = "+getWidth()+"]";
+    }
+
+
+
+}
