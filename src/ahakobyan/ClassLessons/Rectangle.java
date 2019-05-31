fbab2f314c7a93ac79d8d6e3a11633300b5a3348
AbulHakobyan <abulhakobyan20@gmail.com>
2019-05-31 21:01:44 +0400

    Classes Employee_Rectangle_Invoiceitem

---
+++ src/ahakobyan/ClassLessons/Rectangle.java
@@ -0,0 +1,45 @@
+package ahakobyan.ClassLessons;
+
+public class Rectangle {
+
+    private float length = 1.0f;
+    private float width = 1.0f;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width =width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public double getArea(double area) {
+        area = length * width;
+        return area;
+    }
+
+    public double getPerimeter(double perimeter) {
+        perimeter = 2 * (width + length);
+        return perimeter;
+    }
+
+    public String toString() {
+        return "Rectangle[length :"+ length+", width : "+width+"]";
+    }
+}
