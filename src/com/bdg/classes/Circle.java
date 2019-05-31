98e9a115794e2f5254115e649c40370577942862
nairihakobyan <nairihakobyan992@gmail.com>
2019-06-01 01:03:34 +0400

    Circle simplified

---
+++ src/com/bdg/classes/Circle.java
@@ -0,0 +1,26 @@
+package com.bdg.classes;
+
+public class Circle {
+    private double radius = 1.0;
+    public Circle(){
+
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadious() {
+        return radius;
+    }
+
+    public void setRadious(double radius) {
+        this.radius = radius;
+    }
+    public  double getCircumference(){
+        return this.radius*Math.PI;
+    }
+
+    public String toString(){
+        return "Circle[radius = "+ getRadious()+"]";
+    }
+}
