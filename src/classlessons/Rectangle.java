000fca55a83dc57fc0d7abc15574a0a9a4ff2900
AsArabyan <ahhn-93@mail.ru>
2019-06-01 00:26:29 +0400

    Rectangle

---
+++ src/classlessons/Rectangle.java
@@ -0,0 +1,37 @@
+package classlessons;
+
+public class Rectangle {
+    private float length = 1.0f;
+    private float width = 1.0f;
+
+    public Rectangle() {
+    }
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public double getArea(double area) {
+        area = length * width;
+        return area;
+    }
+    public double getPerimeter(double perimeter) {
+        perimeter = 2 * (width + length);
+        return perimeter;
+    }
+    public String toString() {
+        return "Length: " + this.length + " Width: " + this.width;
+    }
+}
