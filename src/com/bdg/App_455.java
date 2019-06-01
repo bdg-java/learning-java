3f01543b2436e04623f9ecdf77455d36a40c1878
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-06-01 23:57:50 +0400

    Initial commit

---
+++ src/com/bdg/App_455.java
@@ -0,0 +1,31 @@
+package com.bdg;
+
+import java.util.Arrays;
+
+public class App_455 {
+
+    public static void main(String[] args) {
+
+        int matrix [] [] = new int  [] []{
+
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {5, 4, 3, 2, 1}};
+
+          int m = 5;
+          int x = 1;
+          double [] b = new double [m];
+
+          for ( int i = 0; i < m ; m++){
+
+                  b [i] = matrix [i] [i] *   matrix [i] [m - 1 - i];
+
+                  System.out.println(Arrays.toString(b));
+              }
+          }
+
+    }
+    
+
