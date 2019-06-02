9efb1edb0f45c86b3f8526bec7df54d47a427d4d
Tigran Mikayelyan <mikayelyan1991@bk.ru>
2019-06-03 00:19:00 +0400

    Initial commit

---
+++ src/Classed/Date.java
@@ -0,0 +1,52 @@
+package Classed;
+
+public class Date {
+
+    private int [] day = { 1, 31};
+    private int [] month = { 1, 12};
+    private int  [] year = { 1900 , 9999};
+
+    public Date ( int [] day, int [] month, int [] year){
+
+      this.day = day;
+      this.month =month;
+      this.year = year;
+    }
+
+     public int [] getday (){
+        return this.day;
+     }
+
+     public int [] getmonth (){
+        return this.month;
+     }
+
+     public int [] getyear (){
+        return this.year;
+     }
+
+     public void setday ( int [] day ){
+        this.day = day;
+     }
+
+     public void setmonth ( int [] month ){
+
+        this.month = month;
+
+     }
+
+     public void setyear ( int [] year ){
+
+        this.year = year;
+
+     }
+
+     public void setDate ( int [] day, int [] month, int [] year){
+
+     }
+      public String  toString () {
+          return "Date: " + this.day + " , " + this.month + " , " + this.year + ;
+
+      }
+
+}
