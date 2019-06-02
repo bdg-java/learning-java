6b483700096464dc4ff8f5460f3efd16c87725a7
AGharibyan <anigharib@gmail.com>
2019-06-03 04:00:25 +0500

    CircleFinal_update_in package

--- src/com/bdg/agharibyan/uml_tasks/CircleFinal/CircleFinal.java
+++ src/com/bdg/agharibyan/uml_tasks/CircleFinal/CircleFinal.java
@@ -1,4 +1,4 @@
-package com.bdg.agharibyan.uml_tasks;
+package com.bdg.agharibyan.uml_tasks.CircleFinal;
 
 import org.jetbrains.annotations.Contract;
 
@@ -7,14 +7,14 @@ public class CircleFinal {
     private double radius = 1.0;
     private String color = "red";
 
-    @Contract(pure = true)// Idea-n arajarkec, vor import anem
+    @Contract(pure = true)// Idea-n arajarkec, vor import anem - bayc inchi hamar e chem haskanum
     public CircleFinal(){
-    } // William jan, nman datark constructor grelu imasty vorn e, ete aranc grelu el da ashkhatum e?
-    @Contract(pure = true)// Idea-n arajarkec, vor import anem
+    }
+    @Contract(pure = true)// Idea-n arajarkec, vor import anem - bayc inchi hamar e chem haskanum
     public CircleFinal(double radius){
         this.radius = radius;
     }
-    @Contract(pure = true)// Idea-n arajarkec, vor import anem
+    @Contract(pure = true)// Idea-n arajarkec, vor import anem - bayc inchi hamar e chem haskanum
     public CircleFinal(double radius, String color){
         this.radius = radius;
         this.color = color;
@@ -23,15 +23,19 @@ public class CircleFinal {
         return this.radius;
     }
     public String getColor(){
+
         return this.color;
     }
     public void setRadius(double radius){
+
         this.radius = radius;
     }
     public void setColor(String color){
+
         this.color = color;
     }
     public String toString(){
+
         return ("Circle[radius="+ this.radius +", color="+ this.color + "]");
     }
     public double getArea(){
---
+++ src/com/bdg/agharibyan/uml_tasks/CircleFinal/CircleFinalCheckApplication.java
@@ -0,0 +1,32 @@
+package com.bdg.agharibyan.uml_tasks.CircleFinal;
+
+public class CircleFinalCheckApplication {
+    public static void main(String[] args) {
+
+        CircleFinal object1 = new CircleFinal();
+        CircleFinal object2 = new CircleFinal(2.5);
+        CircleFinal object3 = new CircleFinal(5.0, "blue");
+
+        System.out.println("firstObjectRadius:"+ object1.getRadius());
+        System.out.println("secondObjectRadius:"+ object2.getRadius());
+        System.out.println("thirdObjectRadius:"+ object3.getRadius());
+        System.out.println("firstObjectColor:"+ object1.getColor());
+        System.out.println("secondObjectColor:"+ object2.getColor());
+        System.out.println("thirdObjectColor:"+ object3.getColor());
+        System.out.println("firstObject:"+ object1.toString());
+        object1.setRadius(1.1);
+        object1.setColor("white");
+        System.out.println("firstObjectAfterSetter:"+ object1.toString());
+        System.out.println("secondObject:"+ object2.toString());
+        object2.setRadius(2.2);
+        object2.setColor("yellow");
+        System.out.println("secondObjectAfterSetter:"+ object2.toString());
+        System.out.println("thirdObject:"+ object3.toString());
+        object3.setRadius(3.3);
+        object3.setColor("black");
+        System.out.println("thirdObjectAfterSetter:"+ object3.toString());
+        System.out.println("firstObjectArea:"+ object1.getArea());
+        System.out.println("secondObjectArea:"+ object2.getArea());
+        System.out.println("thirdObjectArea:"+ object3.getArea());
+    }
+}
