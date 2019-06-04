8832dc31dfa93c44440b91c2fe1148b0ae660f99
AbulHakobyan <abulhakobyan20@gmail.com>
2019-06-04 17:49:02 +0400

    Question_4

---
+++ src/ahakobyan/ClassLessons/BookDemo.java
@@ -0,0 +1,9 @@
+package ahakobyan.ClassLessons;
+
+public class BookDemo {
+    public static void main(String[] args) {
+
+        Book book = new Book("Developing Java Software", new Author("Russel", "Winderand"),  79.75);
+        System.out.println(book);
+    }
+}
