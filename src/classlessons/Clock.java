d8c5ebfecce056ffdf31dcb8c5c77b2aec17ca8a
AsArabyan <ahhn-93@mail.ru>
2019-06-04 21:38:31 +0400

    Clock

---
+++ src/classlessons/Clock.java
@@ -0,0 +1,67 @@
+package classlessons;
+
+public class Clock {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Clock() {
+        hour = 12;
+        minute = 0;
+        second = 0;
+
+    }
+    public Clock(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public Clock(int TimeInSeconds) {
+        hour = hour / TimeInSeconds;
+        minute = (TimeInSeconds - hour * 3600) / 60;
+        second = (TimeInSeconds - hour * 3600 - minute * 60);
+    }
+    public int getHour(){
+        return this.hour;
+    }
+    public int getMinute(){
+        return this.minute;
+    }
+    public int getSecond(){
+        return this.second;
+    }
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+    public void setSecond(int second) {
+        this.second = second;
+    }
+    public void setClock(int TimeinSeconds) {
+        hour = hour / TimeinSeconds;
+        minute = (TimeinSeconds - hour * 3600) / 60;
+        second = (TimeinSeconds - hour * 3600 - minute * 60);
+    }
+    public Clock tick() {
+        second = this.second + 1;
+        return this;
+    }
+    public void addClock(Clock clock) {
+        hour = this.hour + clock.hour;
+        minute = this.minute + clock.minute;
+        second = this.second + clock.second;
+    }
+    public Clock tickDown() {
+        second = this.second - 1;
+        return this;
+    }
+    public Clock subtractClock(Clock clock) {
+        hour = this.hour - clock.hour;
+        minute = this.minute - clock.minute;
+        second = this.second - clock.second;
+        return this;
+    }
+
+}
