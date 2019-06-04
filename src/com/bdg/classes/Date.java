29f80989ef5e44ef642722bc65597e0ad8b484f1
nairihakobyan <nairihakobyan992@gmail.com>
2019-06-04 17:59:13 +0400

    ...

--- src/com/bdg/classes/Date.java
+++ src/com/bdg/classes/Date.java
@@ -1,42 +1,58 @@
 package com.bdg.classes;
 
 public class Date {
-    private int [] day = {1,31};
-    private int[] month = {1,12};
-    private int[] year = {1900,9999};
+    private int  day ;
+    private int month ;
+    private int year ;
 
-    public Date(int[] day ,int [] month ,int[] year){
+    public Date(int day ,int  month ,int year){
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
 
-    public int[] getDay() {
+    public int getDay() {
         return day;
     }
 
-    public void setDay(int[] day) {
+    public void setDay(int day) {
         this.day = day;
     }
 
-    public int[] getMonth() {
+    public int getMonth() {
         return month;
     }
 
-    public void setMonth(int[] month) {
+    public void setMonth(int month) {
         this.month = month;
     }
 
-    public int[] getYear() {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(int[] year) {
+    public void setYear(int year) {
         this.year = year;
     }
 
     public void setDate(int day ,int month , int year){
-        //to be continue...
+        this.day++;
+        if (this.day >= 31){
+            this.month++;
+            this.day = 1;
+            if (this.month >= 12){
+                this.year++;
+                this.month = 1;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.day < 10 || this.month < 10 ){
+        return "0"+this.day+"/0"+this.month+"/"+this.year;
+        }
+        return  this.day+"/"+this.month+"/"+this.year;
     }
 }
