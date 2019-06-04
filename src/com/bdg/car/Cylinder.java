1eff76ff2bc129c8b23242f9233eecaf1d1e32f8
nairihakobyan <nairihakobyan992@gmail.com>
2019-06-04 18:00:17 +0400

    ...

---
+++ src/com/bdg/car/Cylinder.java
@@ -0,0 +1,20 @@
+package com.bdg.car;
+
+
+/**
+ * @author William Arustamyan
+ */
+
+
+public class Cylinder {
+
+    private double volume;
+
+    public Cylinder(double volume) {
+        this.volume = volume;
+    }
+
+    public double volume() {
+        return this.volume;
+    }
+}
---
+++ src/com/bdg/car/Transmission.java
@@ -0,0 +1,39 @@
+package com.bdg.car;
+
+
+/**
+ * @author William Arustamyan
+ */
+
+
+public class Transmission {
+
+    private int shiftGearsNumber;
+    private int currentShiftGear;
+
+
+    private Transmission(int shiftGearsNumber) {
+        this.shiftGearsNumber = shiftGearsNumber;
+    }
+
+    public static Transmission createTransmission(int shiftGearsNumber) {
+        if (shiftGearsNumber < 0) {
+            return null;
+        }
+        return new Transmission(shiftGearsNumber);
+    }
+
+    public void setShiftGear(int gearNumber) {
+        this.currentShiftGear = gearNumber;
+    }
+
+    public void shiftUp() {
+        this.currentShiftGear = this.currentShiftGear + 1;
+    }
+
+    public void shiftDown() {
+        if (this.shiftGearsNumber != 0) {
+            this.currentShiftGear = this.currentShiftGear - 1;
+        }
+    }
+}
