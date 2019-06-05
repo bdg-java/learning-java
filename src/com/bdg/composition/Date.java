d8aa15f6ac488444f7b4d4d01ba1fc365d86da58
William Arustamyan <williamarustamyan@gmail.com>
2019-06-05 21:44:13 +0400

    Date class composition

---
+++ src/com/bdg/composition/Date.java
@@ -0,0 +1,34 @@
+package com.bdg.composition;
+
+class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    int day() {
+        return this.day;
+    }
+
+    int month() {
+        return this.month;
+    }
+
+    int year() {
+        return this.year;
+    }
+
+    @Override
+    public String toString() {
+        return "Date{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/com/bdg/composition/ValueInRange.java
@@ -0,0 +1,22 @@
+package com.bdg.composition;
+
+public class ValueInRange {
+
+    private int start;
+    private int value;
+    private int end;
+
+    public ValueInRange(int start, int value, int end) {
+        this.start = start;
+        this.value = value;
+        this.end = end;
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public boolean isInRange() {
+        return value >= start && value <= end;
+    }
+}
