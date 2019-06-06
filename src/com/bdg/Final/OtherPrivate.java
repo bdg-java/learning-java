a27e8b6e7f131e052747a6fc229326e20553243f
Seda Hovsepyan <sedahovsepyan8@gmail.com>
2019-06-06 23:56:52 +0400

    FinalTestForMe

---
+++ src/com/bdg/Final/OtherPrivate.java
@@ -0,0 +1,15 @@
+package com.bdg.Final;
+
+public  class OtherPrivate {
+    public static void main(String[] args) {
+        Pravite myObj = new Pravite("John");
+        System.out.println( myObj.lname);
+        System.out.println("Email: " + myObj.email);
+//        System.out.println(myObj.fname);
+//        System.out.println("Age: " + myObj.age);
+        Try objj = new Try(5);
+
+//        objj.x = 5;
+
+    }
+}
---
+++ src/com/bdg/Final/Pravite.java
@@ -0,0 +1,14 @@
+package com.bdg.Final;
+
+ public final class Pravite {
+    private String fname = "John";
+  public   final String lname = "Doe";
+    public String email = "john@doe.com";
+    private int age = 24;
+    boolean x = true;
+
+   public  Pravite (String name){
+        this.fname = name;
+
+    }
+}
