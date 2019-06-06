7cefd3a776fb40e273d06e43d34efe3ad60ee4c7
nairihakobyan <nairihakobyan992@gmail.com>
2019-06-07 01:09:06 +0400

    questions in MovablePoint

---
+++ src/com/bdg/Inheritance/Point/MovablePoint.java
@@ -0,0 +1,62 @@
+package com.bdg.Inheritance.Point;
+
+public class MovablePoint extends Point {
+    private float xSpeed = 0;
+    private float ySpeed = 0;
+
+    public MovablePoint() {
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed,float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public float getSpeed(){
+        xSpeed++;
+        ySpeed++;
+        return xSpeed * ySpeed;
+    }
+    public MovablePoint move(){
+        x += xSpeed;
+        y += ySpeed;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
---
+++ src/com/bdg/Inheritance/Point/Point.java
@@ -0,0 +1,45 @@
+package com.bdg.Inheritance.Point;
+
+public class Point {
+    protected float x = 0;
+    protected float y = 0;
+
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x , float y){
+        this.x = x;
+        this.y = y;
+    }
+    public float getXY(){
+        return x*y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
