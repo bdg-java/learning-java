3d71d269b4ea20ce67ea7eea6b032fb91b43434d
AsArabyan <ahhn-93@mail.ru>
2019-06-08 00:29:02 +0400

    Point

---
+++ src/Inheritance/PointMovablePoint/Point.java
@@ -0,0 +1,36 @@
+package Inheritance.PointMovablePoint;
+
+public class Point {
+    protected float x = 0;
+    protected float y = 0;
+
+    public Point() {
+    }
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public float getX() {
+        return x;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+    public float getY() {
+        return y;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public float [] getXY() {
+        float[] array = {this.x, this.y};
+        return array;
+    }
+    public String toString() {
+        return " X: " + x + " Y: " + y;
+    }
+}
