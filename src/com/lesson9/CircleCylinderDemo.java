31992b6dc52c4c7d29e95355b80bc276721c5427
mmelnichenko <mariamelnichenko23@gmail.com>
2019-06-08 00:05:27 +0400

    CirclederivedCylinder

---
+++ src/com/lesson9/CircleCylinderDemo.java
@@ -0,0 +1,14 @@
+package com.lesson9;
+
+public class CircleCylinderDemo {
+    public static void main(String[] args) {
+        Circle c = new Circle(3);
+        System.out.println(c);
+        Circle c1 = new Circle(5, "yellow");
+        System.out.println(c1);
+        Cylinder c2 = new Cylinder(3, 6);
+        System.out.println(c2);
+
+
+    }
+}
