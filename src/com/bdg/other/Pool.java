7c6c8893ee4393d8e3a8c1328ff83af2ee0bff4c
nairihakobyan <nairihakobyan992@gmail.com>
2019-06-08 00:24:57 +0400

    ...

--- src/com/bdg/other/Pool.java
+++ src/com/bdg/other/Pool.java
@@ -2,10 +2,10 @@ package com.bdg.other;
 
 public class Pool {
     public static void main(String[] args) {
-//        int speed = 1;
-        long waterQty = 100;
+        float speed;
+        double waterQty = 0;
         int count = 0;
-        for (int speed = 1; count <= 60  ; speed*=2,count++) {
+        for ( speed = 0.1f ; count <= 60  ; speed*=2,count++) {
             waterQty += 2*speed;
             System.out.println("Speed is: "+ speed+ ", timer: "+ count+", "+ waterQty+" liters||");
         }
