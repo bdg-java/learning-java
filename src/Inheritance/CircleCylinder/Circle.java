41b5fefef932d0926c10844202c4cffb9035d6ab
AsArabyan <ahhn-93@mail.ru>
2019-06-07 22:10:47 +0400

    Circle

---
+++ src/Inheritance/CircleCylinder/Circle.java
@@ -0,0 +1,44 @@
+package Inheritance.CircleCylinder;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radious) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    public String toString() {
+        return "Circle[" + " radious = " + radius + ", color = " + color + "]";
+    }
+}
+
+
