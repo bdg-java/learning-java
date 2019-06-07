132fe62cf0b330be51b28a43c80ea111b1370816
AGharibyan <anigharib@gmail.com>
2019-06-07 23:33:10 +0500

    inheritance_Circle

---
+++ src/com/bdg/agharibyan/inheritanceHomwork/Circle/Application.java
@@ -0,0 +1,25 @@
+package com.bdg.agharibyan.inheritanceHomwork.Circle;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Circle myCircle = new Circle();
+        Cylinder myCylinder = new Cylinder();
+
+        System.out.println(myCircle.toString());
+        System.out.println("Radius before change:"+ myCircle.getRadius());
+        System.out.println("Color before change:"+ myCircle.getColor());
+        System.out.println("Height before change:"+ myCylinder.getHeight());
+        System.out.println("Area before change:"+ myCircle.getArea());
+        System.out.println("Volume before change"+ myCylinder.getVolume());
+        myCircle.setRadius(2);
+        myCircle.setColor("green");
+        myCylinder.setHeight(5);
+        System.out.println(myCircle.toString());
+        System.out.println("Radius after change:"+ myCircle.getRadius());
+        System.out.println("Color after change:"+ myCircle.getColor());
+        System.out.println("Height after change:"+ myCylinder.getHeight());
+        System.out.println("Area after change:"+ myCircle.getArea());
+        System.out.println("Volume after change"+ myCylinder.getVolume());
+    }
+}
