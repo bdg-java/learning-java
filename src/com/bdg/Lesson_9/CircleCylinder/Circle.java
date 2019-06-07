1693b7a9dbff86f285bec9519e705a3981afe127
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-06-07 14:03:30 +0400

    Circle-Cylinder

---
+++ src/com/bdg/Lesson_9/CircleCylinder/Circle.java
@@ -0,0 +1,44 @@
+package com.bdg.Lesson_9.CircleCylinder;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Color : " + color + "Radius" + radius;
+    }
+}
