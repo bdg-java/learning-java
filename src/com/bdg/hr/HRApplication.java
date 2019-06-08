4b5e2eba782fa2a196292768aa6f1fc5244e021d
William Arustamyan <williamarustamyan@gmail.com>
2019-06-08 13:46:49 +0400

    Main class

---
+++ src/com/bdg/hr/HRApplication.java
@@ -0,0 +1,12 @@
+package com.bdg.hr;
+
+public class HRApplication {
+
+    public static void main(String[] args) {
+
+
+        Employee employee = null;
+
+        new HRManagementTool().addEmployee(employee);
+    }
+}
