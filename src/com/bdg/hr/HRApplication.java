8a34bbf671643b995bcc588dc541fbba6989972f
Nuard <nuard@atabekyan.com>
2019-06-15 09:59:17 +0400

    adding a few methods HRManagment tool

--- src/com/bdg/hr/HRApplication.java
+++ src/com/bdg/hr/HRApplication.java
@@ -6,8 +6,9 @@ public class HRApplication {
 
     public static void main(String[] args) {
 
+
 //        new HRManagementTool().findByProfession(new Profession("Developer"));
-//        Developer developer = new Developer(new SalaryInfo("5181800000124589", 10), "John", "Smith", 10);
+
         Employee developer1 = new Developer(new SalaryInfo("5181800000124589", 100), "John", "Smith", 20);
         Employee developer2 = new Developer(new SalaryInfo("5181800000124588", 200), "Jack", "Black", 21);
         Employee manager1 = new Manager(new SalaryInfo("5181800000124587", 200), "Ann", "Black", 21);
@@ -27,6 +28,31 @@ public class HRApplication {
 //        System.out.println(Arrays.toString((button.removeWithSalaryRange(70, 155))));
 //        System.out.println(Arrays.toString(button.findByCardNumber("5181800000124581")));
         System.out.println(Arrays.toString(button.findEmployeeWithSalaryRange(10, 101)));
+
+
+
+
+
+
+
+
+        //        Shape shape = new Shape();
+//
+//        Triangle triangle = new Triangle();
+//        System.out.println(shape.figureName());
+//        System.out.println(triangle.figureName());
+//
+//        Shape[] shapes = new Shape[5];
+//        shapes[0] = new Triangle();
+//        shapes[1] = new Rectangle();
+//
+//        int total = 0;
+//        for (Shape s : shapes) {
+//            if (s != null) {
+//                total = total + s.area();
+//            }
+//        }
+//        System.out.println("Total area : " + total);
 
     }
 }
