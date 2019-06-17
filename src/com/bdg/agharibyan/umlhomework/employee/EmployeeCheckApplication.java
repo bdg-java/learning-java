998727b981f0098738b9edd4d9371dee70013c36
anigharibyan <anigharib@gmail.com>
2019-06-17 17:24:54 +0400

    Commentel em Employee classy, vorpeszi chkhangari HR Management Tolli ashkhatranqin.

--- src/com/bdg/agharibyan/umlhomework/employee/EmployeeCheckApplication.java
+++ src/com/bdg/agharibyan/umlhomework/employee/EmployeeCheckApplication.java
@@ -1,19 +1,19 @@
-package com.bdg.agharibyan.umlhomework.employee;
-
-public class EmployeeCheckApplication {
-    public static void main(String[] args) {
-
-        Employee object = new Employee(123456789, "Poghos", "Poghosyan", 300000);
-
-        System.out.println(object.getId());
-        System.out.println(object.getFirstName());
-        System.out.println(object.getLastName());
-        System.out.println(object.getName());
-        System.out.println(object.getSalary());
-        object.setSalary(400000);
-        System.out.println(object.getSalary());
-        System.out.println(object.getAnnualSalary());
-        System.out.println(object.raiseSalary(10));
-        System.out.println(object.toString());
-    }
-}
+//package com.bdg.agharibyan.umlhomework.employee;
+//
+//public class EmployeeCheckApplication {
+//    public static void main(String[] args) {
+//
+//        Employee object = new Employee(123456789, "Poghos", "Poghosyan", 300000);
+//
+//        System.out.println(object.getId());
+//        System.out.println(object.getFirstName());
+//        System.out.println(object.getLastName());
+//        System.out.println(object.getName());
+//        System.out.println(object.getSalary());
+//        object.setSalary(400000);
+//        System.out.println(object.getSalary());
+//        System.out.println(object.getAnnualSalary());
+//        System.out.println(object.raiseSalary(10));
+//        System.out.println(object.toString());
+//    }
+//}
