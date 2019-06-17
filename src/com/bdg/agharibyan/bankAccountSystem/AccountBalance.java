5842ccbfc25234dc038f18c176f33217825e3aae
anigharibyan <anigharib@gmail.com>
2019-06-17 21:53:49 +0400

    AccountBalance class implemented

--- src/com/bdg/agharibyan/bankAccountSystem/AccountBalance.java
+++ src/com/bdg/agharibyan/bankAccountSystem/AccountBalance.java
@@ -1,4 +1,13 @@
 package com.bdg.agharibyan.bankAccountSystem;
 
 public class AccountBalance {
+    private int accountBalance;
+
+    private AccountBalance(int accountBalance){
+        this.accountBalance = accountBalance;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
 }
