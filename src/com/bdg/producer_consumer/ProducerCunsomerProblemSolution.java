fe72cf0f2118aad1ea5d1431ddb59c691da72073
nairihakobyan <nairihakobyan992@gmail.com>
2019-08-07 01:35:21 +0400

    Unfinished Producer-Consumer problem

--- src/com/bdg/producer_consumer/ProducerCunsomerProblemSolution.java
+++ src/com/bdg/producer_consumer/ProducerCunsomerProblemSolution.java
@@ -1,5 +1,6 @@
 package src.com.bdg.producer_consumer;
 
+import javax.management.monitor.Monitor;
 import java.util.PriorityQueue;
 
 import java.util.Random;
@@ -13,12 +14,14 @@ public class ProducerCunsomerProblemSolution extends Thread {
 
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < producers.length; i++) {
-            producers[i] = new Produce();
-            consumers[i] = new Consume();
-            producers[i].start();
-            producers[i].setName("p" + (i + 1));
-            consumers[i].setName("c"+(i+1));
+
+            for (int i = 0; i < producers.length; i++) {
+                producers[i] = new Produce();
+                consumers[i] = new Consume();
+                producers[i].start();
+                producers[i].setName("p" + (i + 1));
+                consumers[i].setName("c" + (i + 1));
+
         }
     }
 
@@ -29,34 +32,46 @@ public class ProducerCunsomerProblemSolution extends Thread {
     static class Produce extends Thread {
         @Override
         public void run() {
-            int min = 1;
-            int max = 99;
-            int diff = max - min;
-            Random random = new Random();
-            int num = random.nextInt(diff + 1);
-            num += min;
-            for (int i = 0; i <10 ; i++) {
-                synchronized (this) {
-                     num = random.nextInt(diff + 1);
-                    resource.add(num);
-                    count++;
-                    System.out.println(resource.toString()+" "+count);
-                    if (count==10){
-                        i=0;
-                        consumers[i].run();
+//            while (true) {
+                int min = 1;
+                int max = 99;
+                int diff = max - min;
+                Random random = new Random();
+                int num = random.nextInt(diff + 1);
+                num += min;
+                for (int i = 0; i < 20; i++) {
+                    synchronized (currentThread()) {
+                        num = random.nextInt(diff + 1);
+                        resource.add(num);//poxel poll-i
+                        ProducerCunsomerProblemSolution.holdsLock(resource);
+                        count++;
+
+                        System.out.println(resource.toString() + " " + count);
+                        if (count == 10) {
+                            i = 0;
+                            try {
+                                Thread.currentThread().wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            consumers[i].run();
+                        }
                     }
                 }
             }
         }
-    }
+//    }
 
     static class Consume extends Thread {
         @Override
         public void run() {
-            synchronized (this) {
+            synchronized (resource) {
                 System.out.println("consume");
-                resource.removeAll(resource);
+                resource.poll();//poxel poll-i u haskanal notify-i paymanner@
                 count--;
+                if (count<10) {
+                    Thread.currentThread().notifyAll();
+                }
             }
         }
     }
