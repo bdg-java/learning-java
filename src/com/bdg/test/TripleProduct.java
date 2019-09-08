cc1862e93e7f918d1c09fc58dae5021e6acce0df
anigharibyan <anigharib@gmail.com>
2019-09-08 13:20:44 +0400

    TripleProduct - added second version for solution

--- src/com/bdg/test/TripleProduct.java
+++ src/com/bdg/test/TripleProduct.java
@@ -1,13 +1,15 @@
 package com.bdg.test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class TripleProduct {
 
-    private static int tripleProduct(List<Integer> array){
+    private static int tripleProduct1Version(List<Integer> array){
 
-        int max = array.get(0)*array.get(1)*array.get(2);
+        System.out.println(array);
+        int max = 0;
         for(int i = 0; i < array.size(); i++) {
             for(int j = 1; j < array.size(); j++){
                 for(int k = 2; k < array.size(); k++){
@@ -20,6 +22,23 @@ public class TripleProduct {
         return max;
     }
 
+    private static int tripleProduct2Version(List<Integer> array){
+        array.sort(Comparator.naturalOrder());
+        System.out.println(array);
+        int max = 1;
+        if(array.get(1) < 0){
+            if(array.get(0)*array.get(1)*array.get(array.size()-1) > array.get(array.size()-1)*array.get(array.size()-2)*array.get(array.size()-3)){
+                max = array.get(0)*array.get(1)*array.get(array.size()-1);
+            } else{
+                max = array.get(array.size()-1)*array.get(array.size()-2)*array.get(array.size()-3);
+            }
+        } else{
+            max = array.get(array.size()-1)*array.get(array.size()-2)*array.get(array.size()-3);
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
         List<Integer> array = new ArrayList<>();
@@ -27,7 +46,7 @@ public class TripleProduct {
         array.add(6);
         array.add(0);
         array.add(3);
-        array.add(-5);
+        array.add(5);
         array.add(-6);
 
         boolean isOk = false;
@@ -41,7 +60,8 @@ public class TripleProduct {
         }
 
         if(isOk){
-            System.out.println(tripleProduct(array));
+            System.out.println(tripleProduct1Version(array)); // first version
+            System.out.println(tripleProduct2Version(array)); // second version
         }
     }
 }
